2c566cb5f7e822df50ada6153e3db6bda9978af4
Palak <palakagrawal020@gmail.com>
2023-09-18 23:06:48 +0530

    Add CSS styling and Store 2 books in array

--- script.js
+++ script.js
@@ -6,15 +6,24 @@ function Book(title, author, pages) {
     this.pages = pages;
 }
 
-function addBookToLibrary() {
-    let bookNo = prompt("Enter the number of books you want to enter");
-    let number = parseInt(bookNo, 10);
-    for(let i=0; i<number; i++){
-        let newBook = new Book(prompt("Enter the title"), prompt("Enter the author"), prompt("Enter the number of pages"));
-        myLibrary.push(newBook);
-        alert("Book added");
-    }
-}
+let book1 = new Book('A', 'B', 2);
+let book2 = new Book('c', 'D', 3);
 
-addBookToLibrary();
+myLibrary.push(book1);
+myLibrary.push(book2);
+
 console.log(myLibrary);
+
+// function addBookToLibrary() {
+//     let bookNo = prompt("Enter the number of books you want to enter");
+//     let number = parseInt(bookNo, 10);
+//     for(let i=0; i<number; i++){
+//         let newBook = new Book(prompt("Enter the title"), prompt("Enter the author"), prompt("Enter the number of pages"));
+//         myLibrary.push(newBook);
+//         alert("Book added");
+//     }
+// }
+
+// addBookToLibrary();
+// console.log(myLibrary);
+
